8a4c2cbe7514af01856bbe2ce2a6750be0b42954
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-15 17:58:51 -0800

    Add finished contains duplicate code

--- src/main/java/NeetCode150/Arrays/ContainsDuplicate.java
+++ src/main/java/NeetCode150/Arrays/ContainsDuplicate.java
@@ -1,4 +1,30 @@
 package NeetCode150.Arrays;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class ContainsDuplicate {
+
+
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numsUsed = new HashSet<>();
+        for(int i = 0; i<nums.length; i++){
+            if(numsUsed.contains(nums[i])) {
+                return true;
+            }
+            numsUsed.add(nums[i]);
+        }
+        return  false;
+    }
+
+
+    public static void main(String[] args) {
+        int[] num1 = {1, 2, 3, 1};
+        int[] num2 = {1,2,3,4};
+        int[] num3 = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(num1));
+    }
 }
