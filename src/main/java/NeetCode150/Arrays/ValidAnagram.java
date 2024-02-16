16fe27bbf5a0f6f188a5302cee00b52cae8ccb61
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-15 19:05:19 -0800

    Add ValidAnagram problem with solution

--- src/main/java/NeetCode150/Arrays/ValidAnagram.java
+++ src/main/java/NeetCode150/Arrays/ValidAnagram.java
@@ -27,6 +27,7 @@ Follow up: What if the inputs contain Unicode characters? How would you adapt yo
  */
 public class ValidAnagram {
 
+    //This is the fastest way to accomplish
     public static boolean isAnagram(String x, String y) {
         char[] wordX = x.toCharArray();
         char[] wordY = y.toCharArray();
@@ -37,14 +38,37 @@ public class ValidAnagram {
         return Arrays.equals(wordX, wordY);
     }
 
+    // Slower, created for practice using Hashmap.
+    public static boolean isAnagramUsingHash(String x, String y){
+        HashMap <Character, Integer> Count = new HashMap<>();
+
+        for(char letter : x.toCharArray()){
+            Count.put(letter, Count.getOrDefault(letter, 0) + 1);
+        }
+        for(char letter : y.toCharArray()){
+            Count.put(letter, Count.getOrDefault(letter, 0) - 1);
+        }
+
+        for(int val : Count.values()){
+            if(val !=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+
     public static void main(String[] args) {
         String s = "anagram";
         String t = "nagaram";
         String x = "rat";
         String y = "car";
 
-        System.out.println(isAnagram(s,t));
-        System.out.println(isAnagram(x,y));
+        System.out.println(isAnagramUsingHash(s,t));
+        System.out.println(isAnagramUsingHash(x,y));
 
 
     }
